edc419e4dadb3cc7533a9479a5cf357abdee5416
Koichi Kiyokawa <koichi20110068@gmail.com>
2022-10-27 22:00:41 +0900

    chore: add react + css modules

---
+++ astro-react-css-modules/src/features/Home/pages/Home/index.tsx
@@ -0,0 +1,10 @@
+import styles from "./style.module.css"
+
+const Home = () => (
+  <div>
+    <h1 className={styles.title}>Home page</h1>
+    <h2 className={styles.sub_title}>sub title</h2>
+  </div>
+)
+
+export default Home
